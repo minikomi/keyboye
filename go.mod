da1538aa95cb117a2d8416d462a7932616baa17c
Adam Moore <nerdfunk@gmail.com>
2019-05-09 17:10:50 +0900

    first try with go midi, sdl2. random test pattern drawing & note sending

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/minikomi/keyboye
 go 1.12
 
 require (
-	github.com/gdamore/tcell v1.1.2
-	rsc.io/quote v1.5.2
+	github.com/gomidi/connect v0.11.1
+	github.com/gomidi/midi v1.6.0
+	github.com/minikomi/rtmididrv v0.1.28
+	github.com/veandco/go-sdl2 v0.3.0
 )
--- main.go
+++ main.go
@@ -2,120 +2,242 @@ package main
 
 import (
 	"fmt"
-	"github.com/gdamore/tcell"
+	"github.com/gomidi/connect"
+	"github.com/gomidi/midi"
+	"github.com/gomidi/midi/midimessage/channel"
+	//	. "github.com/gomidi/midi/midimessage/channel"
+	"github.com/gomidi/midi/midiwriter"
+	driver "github.com/minikomi/rtmididrv"
+	"github.com/veandco/go-sdl2/sdl"
+	"io"
 	"os"
 )
 
+var winTitle string = "Go-SDL2 Render"
+var winWidth, winHeight int32 = 800, 600
+
 type state struct {
-	x, y int
 }
 
-func intmin(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+var keyMap = map[sdl.Keycode]uint8{
+	sdl.K_a: 63,
+	sdl.K_s: 64,
+	sdl.K_d: 65,
+	sdl.K_f: 66,
 }
 
-func intmax(a, b int) int {
-	if a > b {
-		return a
+func handleKeyEvent(ev *sdl.KeyboardEvent, wr *Writer) {
+	fmt.Printf("[%d ms] Keyboard\ttype:%d\tsym:%c\tmodifiers:%d\tstate:%d\trepeat:%d\n",
+		ev.Timestamp, ev.Type, ev.Keysym.Sym, ev.Keysym.Mod, ev.State, ev.Repeat)
+	note, ok := keyMap[ev.Keysym.Sym]
+
+	if ok {
+		fmt.Println(note)
+		if ev.State == 1 {
+			if ev.Repeat != 1 {
+				wr.NoteOn(note, 90)
+			}
+		} else {
+			wr.NoteOff(note)
+		}
+	} else {
+		fmt.Printf("%d %d\n", sdl.GetScancodeFromKey(ev.Keysym.Sym), sdl.K_a)
 	}
-	return b
 }
 
-func clamp(v, l, u int) int {
-	return intmax(l, intmin(v, u))
+func draw(renderer *sdl.Renderer) {
+	var points []sdl.Point
+	var rect sdl.Rect
+	var rects []sdl.Rect
+
+	renderer.Clear()
+
+	renderer.SetDrawColor(255, 255, 255, 255)
+	renderer.DrawPoint(150, 300)
+
+	renderer.SetDrawColor(0, 0, 255, 255)
+	renderer.DrawLine(0, 0, 200, 200)
+
+	points = []sdl.Point{{0, 0}, {100, 300}, {100, 300}, {200, 0}}
+	renderer.SetDrawColor(255, 255, 0, 255)
+	renderer.DrawLines(points)
+
+	rect = sdl.Rect{300, 0, 200, 200}
+	renderer.SetDrawColor(255, 0, 0, 255)
+	renderer.DrawRect(&rect)
+
+	rects = []sdl.Rect{{400, 400, 100, 100}, {550, 350, 200, 200}}
+	renderer.SetDrawColor(0, 255, 255, 255)
+	renderer.DrawRects(rects)
+
+	rect = sdl.Rect{250, 250, 200, 200}
+	renderer.SetDrawColor(0, 255, 0, 255)
+	renderer.FillRect(&rect)
+
+	rects = []sdl.Rect{{500, 300, 100, 100}, {200, 300, 200, 200}}
+	renderer.SetDrawColor(255, 0, 255, 255)
+	renderer.FillRects(rects)
+
+	renderer.Present()
+
 }
 
-func move(sc tcell.Screen, st state, dx int, dy int) state {
-	w, h := sc.Size()
-	switch {
-	case st.x == 0 && dx == -1:
-		st.x = w - 1
-		move(sc, st, 0, -1)
-	case st.x == w-1 && dx == 1:
-		st.x = 0
-		move(sc, st, 0, 1)
-	default:
-		st.x = clamp(st.x+dx, 0, w)
-		st.y = clamp(st.y+dy, 1, h)
+func must(err error) {
+	if err != nil {
+		panic(err.Error())
 	}
-	return st
 }
 
-func initializeScreen() (sc tcell.Screen) {
-
-	sc, e := tcell.NewScreen()
-
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
-		os.Exit(1)
-	}
-
-	if e = sc.Init(); e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
-		os.Exit(1)
-	}
-
-	sc.Clear()
-	sc.Sync()
-
-	return sc
+type midiWriter struct {
+	wr              midi.Writer
+	ch              channel.Channel
+	noteState       [16][128]bool
+	noConsolidation bool
 }
 
-func drawStateDebug(sc tcell.Screen, st state) {
-	guitxt := fmt.Sprintf("%d x %d", st.x, st.y)
-	w, _ := sc.Size()
-	for i := 0; i < w; i++ {
-		sc.SetContent(i, 0, ' ', nil, 0)
-	}
-	for i, r := range guitxt {
-		sc.SetContent(i, 0, r, nil, 0)
-	}
-	sc.Sync()
+type Writer struct {
+	*midiWriter
 }
 
-func drawKey(sc tcell.Screen, st state, ev *tcell.EventKey) {
-	sc.SetContent(st.x, st.y, ev.Rune(), nil, 0)
-	sc.Sync()
+func NewWriter(dest io.Writer, options ...midiwriter.Option) *Writer {
+	options = append(
+		[]midiwriter.Option{
+			midiwriter.NoRunningStatus(),
+		}, options...)
+
+	wr := midiwriter.New(dest, options...)
+	return &Writer{&midiWriter{wr: wr, ch: channel.Channel0}}
+}
+
+type outWriter struct {
+	out connect.Out
+}
+
+func (w *outWriter) Write(b []byte) (int, error) {
+	return len(b), w.out.Send(b)
+}
+
+func writeTo(out connect.Out) *Writer {
+	return NewWriter(&outWriter{out})
+}
+
+func (w *midiWriter) NoteOn(key, veloctiy uint8) error {
+	return w.Write(w.ch.NoteOn(key, veloctiy))
+}
+
+func (w *midiWriter) NoteOff(key uint8) error {
+	return w.Write(w.ch.NoteOff(key))
+}
+
+func (w *midiWriter) Write(msg midi.Message) error {
+	if w.noConsolidation {
+		return w.wr.Write(msg)
+	}
+	switch m := msg.(type) {
+	case channel.NoteOn:
+		if m.Velocity() > 0 && w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note already running.", msg)
+		}
+		if m.Velocity() == 0 && !w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note is not running.", msg)
+		}
+		w.noteState[m.Channel()][m.Key()] = m.Velocity() > 0
+	case channel.NoteOff:
+		if !w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note is not running.", msg)
+		}
+		w.noteState[m.Channel()][m.Key()] = false
+	case channel.NoteOffVelocity:
+		if !w.noteState[m.Channel()][m.Key()] {
+			return fmt.Errorf("can't write %s. note is not running.", msg)
+		}
+		w.noteState[m.Channel()][m.Key()] = false
+	}
+	return w.wr.Write(msg)
+}
+
+func run() int {
+
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+	var window *sdl.Window
+	var renderer *sdl.Renderer
+	window, err := sdl.CreateWindow(winTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		winWidth, winHeight, sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create window: %s\n", err)
+		return 1
+	}
+	defer window.Destroy()
+	renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
+		return 2
+	}
+
+	defer renderer.Destroy()
+
+	draw(renderer)
+
+	// midi
+	drv, err := driver.New()
+	must(err)
+	defer drv.Close()
+
+	ins, err := drv.Ins()
+	must(err)
+
+	outs, err := drv.Outs()
+	must(err)
+
+	if len(os.Args) == 2 && os.Args[1] == "list" {
+		printInPorts(ins)
+		printOutPorts(outs)
+		return 0
+	}
+
+	out := outs[0]
+	out.Open()
+	wr := writeTo(out)
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch ev := event.(type) {
+			case *sdl.KeyboardEvent:
+				handleKeyEvent(ev, wr)
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
+	return 0
+}
+
+func printPort(port connect.Port) {
+	fmt.Printf("[%v] %s\n", port.Number(), port.String())
+}
+
+func printInPorts(ports []connect.In) {
+	fmt.Printf("MIDI IN Ports\n")
+	for _, port := range ports {
+		printPort(port)
+	}
+	fmt.Printf("\n\n")
+}
+
+func printOutPorts(ports []connect.Out) {
+	fmt.Printf("MIDI OUT Ports\n")
+	for _, port := range ports {
+		printPort(port)
+	}
+	fmt.Printf("\n\n")
 }
 
 func main() {
-	sc := initializeScreen()
-	quit := make(chan struct{})
-
-	st := state{0, 1}
-
-	go func() {
-		for {
-			sc.ShowCursor(st.x, st.y)
-			drawStateDebug(sc, st)
-			ev := sc.PollEvent()
-			switch ev := ev.(type) {
-			case *tcell.EventKey:
-				switch ev.Key() {
-				case tcell.KeyEscape, tcell.KeyEnter:
-					close(quit)
-					return
-				case tcell.KeyUp:
-					st = move(sc, st, 0, -1)
-				case tcell.KeyDown:
-					st = move(sc, st, 0, 1)
-				case tcell.KeyLeft:
-					st = move(sc, st, -1, 0)
-				case tcell.KeyRight:
-					st = move(sc, st, 1, 0)
-				default:
-					drawKey(sc, st, ev)
-					st = move(sc, st, 1, 0)
-				}
-			case *tcell.EventResize:
-				sc.Sync()
-			}
-		}
-	}()
-
-	<-quit
-	sc.Fini()
+	os.Exit(run())
 }
